230f4d05eaafaf13fb3d8b201a5f25180a6ce8eb
Sergey Petrunin <neyasut@gmail.com>
2016-01-20 13:01:57 +0200

    add initial models and templates

---
+++ app/pods/movies/model.js
@@ -0,0 +1,10 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  title: DS.attr('string'),
+  rating: DS.attr('number'),
+  userRating: DS.attr('number'),
+  year: DS.attr('number'),
+  user: DS.belongsTo('user'),
+  // directors: DS.hasMany('director', { async: true })
+});
