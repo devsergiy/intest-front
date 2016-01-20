3e45fb3d99e8de8507edf36feb944242ef648706
Sergey Petrunin <neyasut@gmail.com>
2016-01-20 12:59:16 +0200

    configure content policy

--- config/environment.js
+++ config/environment.js
@@ -17,6 +17,16 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
+    },
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src':  "'self'",
+      'font-src':    "'self' data:",
+      'connect-src': "'self' localhost:3000",
+      'img-src':     "'self'",
+      'style-src':   "'self'",
+      'media-src':   "'self'"
     }
   };
 
